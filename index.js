3e2a658ea5781819b5028f0cd304da964e721efc
Brett Jackson <brett@brettjackson.org>
2016-03-13 22:04:23 -0500

    docker

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var path = require("path");
 var fs = require('fs');
 
 app.get('/', function (req, res) {
+  console.log('New visitor.');
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 
