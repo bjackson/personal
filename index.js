00732eee668b9d64c68af58822da46f6195b71f1
Brett Jackson <brett@brettjackson.org>
2016-04-15 16:56:10 -0500

    added image

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 
+app.get('/avatar', function (req, res) {
+  res.sendFile(path.join(__dirname, '/facetranspo.png'))
+});
+
 app.listen(9000, function () {
   console.log('Example app listening on port 9000!');
 });
